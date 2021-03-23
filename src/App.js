8418200eb5e10fc2e0a08c0bc73ad564c7f4be65
patedwards <pat.edwards@pm.me>
2021-03-23 16:59:51 -0700

    got mapbox GL working inside a component with hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React, { useEffect, useRef, useState } from "react";
-
-import mapboxgl from "mapbox-gl";
-import './App.css';
-
-const token = "pk.eyJ1IjoicGF0d2FyZHMiLCJhIjoiY2tmZzYwenZ4MDRvaDJxcTljc3EyNWxlYSJ9.cxQNQOhFMdUbgWSoaAPrng"
-
-const Map2 = () => (<p>Hello</p>)
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
+import "mapbox-gl/dist/mapbox-gl.css";
 
 const styles = {
   width: "100vw",
@@ -13,38 +8,77 @@ const styles = {
   position: "absolute"
 };
 
+// Using https://sparkgeo.com/blog/build-a-react-mapboxgl-component-with-hooks/
 
-const Map = () => {
-  const [map, setMap] = useState(null);
-  const mapContainer = useRef(null);
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
-  useEffect(() => {
-    mapboxgl.accessToken =
-      "pk.eyJ1IjoiYnJpYW5iYW5jcm9mdCIsImEiOiJsVGVnMXFzIn0.7ldhVh3Ppsgv4lCYs65UdA";
-    const initializeMap = ({ setMap, mapContainer }) => {
-      const map = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
-        center: [0, 0],
-        zoom: 5
-      });
-
-      map.on("load", () => {
-        setMap(map);
-        map.resize();
-      });
-    };
-
-    if (!map) initializeMap({ setMap, mapContainer });
-  }, [map]);
-
-  return <div ref={el => (mapContainer.current = el)} style={styles} />;
-};
-
-function App() {
-  return (
-    <Map/>
-  );
+const addDraggablePoint = (map, point, color) => {
+  return new mapboxgl.Marker({
+    color,
+    draggable: true
+  }).setLngLat(point)
+  .addTo(map);
 }
 
-export default App;
+const Map = () => {
+  const mapContainerRef = useRef(null);
+
+  const [lng, setLng] = useState(-74.5);
+  const [lat, setLat] = useState(40.1);
+  const [zoom, setZoom] = useState(9);
+
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng, lat],
+      zoom: zoom
+    });
+
+    map.on('load', () => {
+      // TODO Add Source
+      // TODO Add layers for lines
+      // TODO Add draggable point
+      // TODO Add a geocoder
+      // TODO Add results to map
+      // TODO Calculate routes
+      // TODO Add animation function
+      // TODO Add functions for querying POI
+      // Set options
+    
+      
+    
+      var src_point = addDraggablePoint(map, [-74.5, 40], "#FFFFFF")
+      var destination_point = addDraggablePoint(map, [-74.5, 40.1], "#fc03d3")
+    
+      const request_url = "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/-73.9913719052481%2C40.73419007010716%3B-73.98619375981075%2C40.73399458227968?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1IjoicGF0d2FyZHMiLCJhIjoiY2tmZzYwenZ4MDRvaDJxcTljc3EyNWxlYSJ9.cxQNQOhFMdUbgWSoaAPrng"
+      const response = fetch(request_url)
+                        .then(response => response.json())
+                        .then(data => console.log(data))
+    });
+
+    // Add navigation control (the +/- zoom buttons)
+    //map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+    /*
+    map.on('move', () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });
+    */
+    
+
+    // Clean up on unmount
+    return () => map.remove();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <div>
+      <div className='map-container' ref={mapContainerRef} style={styles} />
+    </div>
+  );
+};
+
+export default Map;
