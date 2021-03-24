21d92f0b83dfa84c5f5ce7d2016901a1f5a56a33
patedwards <pat.edwards@pm.me>
2021-03-23 21:46:50 -0700

    Some progress with the routing

--- src/App.js
+++ src/App.js
@@ -1,84 +1,91 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-import "mapbox-gl/dist/mapbox-gl.css";
+import Map from "./Map";
+import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
+import Task from "./Task";
+import { useState, useEffect } from "react";
 
-const styles = {
-  width: "100vw",
-  height: "calc(100vh - 80px)",
-  position: "absolute"
-};
+const drawerWidth = 240;
 
-// Using https://sparkgeo.com/blog/build-a-react-mapboxgl-component-with-hooks/
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+}));
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
 
-const addDraggablePoint = (map, point, color) => {
-  return new mapboxgl.Marker({
-    color,
-    draggable: true
-  }).setLngLat(point)
-  .addTo(map);
+
+const Controls = (props) => {
+  const classes = useStyles();
+  return (
+    <Drawer anchor={"left"} open={true} variant="persistent" className={classes.drawer} classes={{
+      paper: classes.drawerPaper,
+    }}>
+      {props.tasks.map((task) => <Task title={task.name} onSelect={task.onSelect}/>)}
+    </Drawer>
+  )
 }
 
-const Map = () => {
-  const mapContainerRef = useRef(null);
+const App = () => {
+  const [mapTools, setMapTools] = useState([null])
+  const [routes, setRoutes] = useState([])
+  //const [routeEndpoint, setRouteEndpoint] = useState([-74.5, 40])
+  //const [routeStartpoint, setRouteStartpoint] = useState([-74.5, 40.1])
 
-  const [lng, setLng] = useState(-74.5);
-  const [lat, setLat] = useState(40.1);
-  const [zoom, setZoom] = useState(9);
+  const [activeTask, setActiveTask] = useState(null)
 
-  // Initialize map when component mounts
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [lng, lat],
-      zoom: zoom
-    });
+  const actions = {
+ 
+  }
+  
+  const tasks = [
+    {
+      name: "Choose starting point",
+      onSelect: () => {
+        setActiveTask("get-start");
+      },
+    },
+    {
+      name: "Choose destination",
+      onSelect: () => {
+        setActiveTask("get-end");
 
-    map.on('load', () => {
-      // TODO Add Source
-      // TODO Add layers for lines
-      // TODO Add draggable point
-      // TODO Add a geocoder
-      // TODO Add results to map
-      // TODO Calculate routes
-      // TODO Add animation function
-      // TODO Add functions for querying POI
-      // Set options
-    
-      
-    
-      var src_point = addDraggablePoint(map, [-74.5, 40], "#FFFFFF")
-      var destination_point = addDraggablePoint(map, [-74.5, 40.1], "#fc03d3")
-    
-      const request_url = "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/-73.9913719052481%2C40.73419007010716%3B-73.98619375981075%2C40.73399458227968?alternatives=true&geometries=geojson&steps=true&access_token=pk.eyJ1IjoicGF0d2FyZHMiLCJhIjoiY2tmZzYwenZ4MDRvaDJxcTljc3EyNWxlYSJ9.cxQNQOhFMdUbgWSoaAPrng"
-      const response = fetch(request_url)
-                        .then(response => response.json())
-                        .then(data => console.log(data))
-    });
-
-    // Add navigation control (the +/- zoom buttons)
-    //map.addControl(new mapboxgl.NavigationControl(), 'top-right');
-    /*
-    map.on('move', () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
-    });
-    */
-    
-
-    // Clean up on unmount
-    return () => map.remove();
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+      },
+    },
+    {
+      name: "Calculate routes",
+    },
+    {
+      name: "Choose route"
+    },
+    {
+      name: "Route preview"
+    }
+  ]
 
   return (
     <div>
-      <div className='map-container' ref={mapContainerRef} style={styles} />
+      <Map activeTask={activeTask}/>
+      <Controls tasks={tasks}/>
     </div>
-  );
+  )
 };
 
-export default Map;
+export default App;
